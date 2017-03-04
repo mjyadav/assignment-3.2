f7174584d2816afd8df99428627102c84177d9a2
mjyadav <mrityunjay.yadav0007@gmail.com>
2017-03-04 12:56:12 +0530

    assignment3.2
    assignment3.2

---
+++ src/Example.java
@@ -0,0 +1,12 @@
+//What would happen when the following is compiled and executed.
+class Example {  // class name
+	int x;       // integer declare
+	int y;
+	String name;
+
+	public static void main(String args[]) {   //main method
+		Example pnt = new Example();  // creating object
+		System.out.println("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);   //output
+	}
+
+}
